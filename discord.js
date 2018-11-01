5b75dfd7bd7f4b37e3de7b57e06c7782e633a7f7
discord <43854305+hgfhjtfuy@users.noreply.github.com>
2018-11-01 21:42:05 +0200

    Update discord.js

--- discord.js
+++ discord.js
@@ -4,3 +4,18 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+
+
+
+
+client.on ("guildMemberAdd", member => {
+
+   var role = member.guild.roles.find ("name", "• OmEGa » Member");
+   member.addRole (role);
+
+})
+
+client.on ("guildMemberRemove", member => {
+
+})
